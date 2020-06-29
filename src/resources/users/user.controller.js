1eb135188aa8da69dca702ebade2d3277a5257a3
bemijonathan <bemijonathan@gmail.com>
2020-06-29 07:43:38 +0100

    removed unnessary files

--- src/resources/users/user.controller.js
+++ src/resources/users/user.controller.js
@@ -1,4 +1,12 @@
-import crudController from "../utils/crud.js";
+import { crudControllers } from "../../utils/crud.js";
 import { User } from "./user.model";
 
-export default crudControllers(Post);
+export default {
+    ...crudControllers(User),
+    getCount(_, res) {
+        let count = User.count()
+        return res.status(200).send({
+            count
+        })
+    }
+};
--- src/resources/users/user.route.js
+++ src/resources/users/user.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import controller from "./user.controller.js";
+import { verifyPaystack } from "../../utils/paystack.js";
 
 const router = Router();
 
 router
-  .route("users/")
-  .get("/", controller.getMany)
-  .get("count", controller.getCount);
+  .route("/")
+  // .get(controller.getMany)
+  .post(verifyPaystack, controller.createOne);
+
+router.route("count")
+  .get(controller.getCount);
