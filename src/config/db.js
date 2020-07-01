79e8c3f2477284f14e74a1de05d4219ef3f8c2da
jonathan <34762800+bemijonathan@users.noreply.github.com>
2020-07-01 06:09:54 +0100

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ export const dbConnection = () => {
     mongoose.Promise = global.Promise;
     // mongoose.connect('mongodb://localhost:27017/glitz_', { useNewUrlParser: true })
     mongoose.connect(
-        'mongodb+srv://jona:jona@cluster0.qiede.mongodb.net/glitz?retryWrites=true&w=majority',
+        process.env.MONGO,
         { useNewUrlParser: true }
     );
 };
