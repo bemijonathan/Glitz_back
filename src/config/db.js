cbb84f94bd59930de044f9cd14947e08c9558504
bemijonathan <bemijonathan@gmail.com>
2020-07-01 05:53:18 +0100

    moved db to production

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const dbConnection = () => {
-    mongoose.Promise = global.Promise
-    mongoose.connect('mongodb://localhost:27017/glitz_', { useNewUrlParser: true })
-}
+    mongoose.Promise = global.Promise;
+    // mongoose.connect('mongodb://localhost:27017/glitz_', { useNewUrlParser: true })
+    mongoose.connect(
+        'mongodb+srv://jona:jona@cluster0.qiede.mongodb.net/glitz?retryWrites=true&w=majority',
+        { useNewUrlParser: true }
+    );
+};
